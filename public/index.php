40995380a6e656e6f25dd8a805dd5a51055ed120
clarenceevanw <clarenceevan0907@gmail.com>
2025-11-23 11:33:00 +0700

    fix: directory required

--- public/index.php
+++ public/index.php
@@ -1,11 +1,10 @@
 <?php
-require_once __DIR__ . "/config/Database.php";
-require_once __DIR__ . "/core/BaseModel.php";
-require_once __DIR__ . "/core/BaseController.php";
-require_once __DIR__ . "/core/Middleware.php";
-require_once __DIR__ . "/core/Router.php";
+require_once __DIR__ . "/../config/Database.php";
+require_once __DIR__ . "/../app/Models/BaseModel.php";
+require_once __DIR__ . "/../app/Controllers/BaseController.php";
+require_once __DIR__ . "/../app/Middleware/Middleware.php";
+require_once __DIR__ . "/../app/Router.php";
 
-require_once __DIR__ . "/routes/web.php";
+require_once __DIR__ . "/../routes/web.php";
 
-// jalankan router
 Router::run();
