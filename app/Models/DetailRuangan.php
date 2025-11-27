dd96960088e3df5167a6e0c0b058624562b0c15d
clarenceevanw <clarenceevan0907@gmail.com>
2025-11-27 07:21:19 +0700

    feat: getIsiRuangan

--- app/Models/DetailRuangan.php
+++ app/Models/DetailRuangan.php
@@ -7,4 +7,15 @@ class DetailRuangan extends BaseModel
     {
         return parent::__construct();
     }
+
+    public function getIsiRuangan($id_ruangan) {
+        $sql = "SELECT dr.*, b.nama_barang, dt.expired_date 
+                FROM detail_ruangan dr
+                JOIN detail_transaksi dt ON dr.id_detail_transaksi = dt.id_detail_transaksi
+                JOIN barang b ON dt.id_barang = b.id_barang
+                WHERE dr.id_ruangan = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id_ruangan]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
