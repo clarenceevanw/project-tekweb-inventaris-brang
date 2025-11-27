13d6d03f7f6ec7eaca1b165908cf7c740023dc03
Jonathan Christian <jonathan.christian211205@gmail.com>
2025-11-27 14:16:56 +0700

    feat: add routes for auth

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,18 @@
 <?php
 require_once __DIR__ . '/../app/Controllers/HomeController.php';
+require_once __DIR__ . '/../app/Controllers/AuthController.php';
 
 Router::get("/", "HomeController@index");
+
+// Auth Routes
+Router::get("/login/admin", "AuthController@showLoginAdmin");
+Router::post("/login/admin", "AuthController@loginAdmin");
+Router::get("/login/mitra", "AuthController@showLoginMitra");
+Router::post("/login/mitra", "AuthController@loginMitra");
+
+Router::get("/signup/admin", "AuthController@showSignupAdmin");
+Router::post("/signup/admin", "AuthController@signupAdmin");
+Router::get("/signup/mitra", "AuthController@showSignupMitra");
+Router::post("/signup/mitra", "AuthController@signupMitra");
+
+Router::get("/logout", "AuthController@logout");
