0882a43ebb1e556603682cbc416121c6e563f096
Jonathan Christian <jonathan.christian211205@gmail.com>
2025-11-27 11:30:41 +0700

    fix: signup admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../Utils/UUID.php';
 
 class Admin extends BaseModel {
     protected $table = 'admin';
@@ -11,8 +12,28 @@ class Admin extends BaseModel {
         return $this->find('username_admin', $username);
     }
 
-    public function signUp($data) {
-        $data['password_admin'] = password_hash($data['password_admin'], PASSWORD_DEFAULT);
-        return $this->insert($data);
+    public function signUpWithGudang($adminData, $gudangData) {
+        $this->db->beginTransaction();
+        try {
+            $id_gudang = generate_uuid();
+            $stmtGudang = $this->db->prepare("INSERT INTO gudang (id_gudang, nama_gudang, lokasi_gudang) VALUES (?, ?, ?)");
+            $stmtGudang->execute([$id_gudang, $gudangData['nama_gudang'], $gudangData['lokasi_gudang']]);
+            
+            $adminData['id_admin'] = generate_uuid();
+            $adminData['id_gudang'] = $id_gudang;
+            $adminData['password_admin'] = password_hash($adminData['password_admin'], PASSWORD_DEFAULT);
+            $this->insert($adminData);
+            
+            $this->db->commit();
+            return true;
+        } catch (Exception $e) {
+            $this->db->rollBack();
+            return false;
+        }
+    }
+
+    public function addAdminToGudang($adminData) {
+        $adminData['password_admin'] = password_hash($adminData['password_admin'], PASSWORD_DEFAULT);
+        return $this->insert($adminData);
     }
 }
