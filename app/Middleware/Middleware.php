c216d52cd2d8c22b679a5fa344b51eac341ceea4
clarenceevanw <clarenceevan0907@gmail.com>
2025-11-28 18:09:23 +0700

    fix: middleware for auth and admin

--- app/Middleware/Middleware.php
+++ app/Middleware/Middleware.php
@@ -1,18 +1,18 @@
 <?php
 class Middleware {
     public static function auth($next) {
-        session_start();
         if (!isset($_SESSION['user'])) {
-            header("Location: /login");
+            header("Location: /login/mitra");
+            $_SESSION['flash']['error'] = 'Anda belum login!';
             exit;
         }
         $next();
     }
 
     public static function admin($next) {
-        session_start();
         if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "admin") {
-            header("Location: /forbidden");
+            header("Location: /login/admin");
+            $_SESSION['flash']['error'] = 'Anda belum login!';
             exit;
         }
         $next();
