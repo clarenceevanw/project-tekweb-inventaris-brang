9f1b6a1fea1c588d0aa1e659eae864ac7e71b8e3
Jonathan Christian <jonathan.christian211205@gmail.com>
2025-12-01 02:11:55 +0700

    fix: table header

--- views/ruangan/batch.php
+++ views/ruangan/batch.php
@@ -18,7 +18,7 @@
                             ID Batch
                         </th>
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Kuantitas
+                            Sisa Stok
                         </th>
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Expired Date
