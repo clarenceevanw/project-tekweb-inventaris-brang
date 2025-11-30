3851833ed08a7ab09e6f5d3800e35fb0e3a81e83
Jonathan Christian <jonathan.christian211205@gmail.com>
2025-12-01 02:07:42 +0700

    feat: add query get barang and get batch by ruangan

--- app/Models/DetailRuangan.php
+++ app/Models/DetailRuangan.php
@@ -8,14 +8,26 @@ class DetailRuangan extends BaseModel
         return parent::__construct();
     }
 
-    public function getIsiRuangan($id_ruangan) {
-        $sql = "SELECT dr.*, b.nama_barang, dt.expired_date 
+    public function getBarangByRuangan($id_ruangan) {
+        $sql = "SELECT b.id_barang, b.nama_barang, SUM(dr.kuantitas_ruangan) as total_stok
                 FROM detail_ruangan dr
                 JOIN detail_transaksi dt ON dr.id_detail_transaksi = dt.id_detail_transaksi
                 JOIN barang b ON dt.id_barang = b.id_barang
-                WHERE dr.id_ruangan = ?";
+                WHERE dr.id_ruangan = ?
+                GROUP BY b.id_barang, b.nama_barang";
         $stmt = $this->db->prepare($sql);
         $stmt->execute([$id_ruangan]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getBatchByRuanganBarang($id_ruangan, $id_barang) {
+        $sql = "SELECT dr.*, dt.expired_date, dt.id_detail_transaksi, b.nama_barang
+                FROM detail_ruangan dr
+                JOIN detail_transaksi dt ON dr.id_detail_transaksi = dt.id_detail_transaksi
+                JOIN barang b ON dt.id_barang = b.id_barang
+                WHERE dr.id_ruangan = ? AND dt.id_barang = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id_ruangan, $id_barang]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
