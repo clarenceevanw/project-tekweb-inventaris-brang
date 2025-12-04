cdf6740ebf5c4bb46b60965e2f5728fb86f49f82
Baksoman <bryanraharjou@gmail.com>
2025-12-04 14:16:05 +0700

    fix: is active sidebar

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -167,7 +167,7 @@
                 </a>
 
                 <!-- 2. Admin -->
-                <a href="/admin/manage-admin" class="relative flex items-center h-12 px-3 rounded-lg transition-colors <?= $isActive('kategori') ? 'bg-indigo-50 text-indigo-600' : 'text-gray-600 hover:bg-gray-100 hover:text-indigo-600' ?>">
+                <a href="/admin/manage-admin" class="relative flex items-center h-12 px-3 rounded-lg transition-colors <?= $isActive('manage-admin') ? 'bg-indigo-50 text-indigo-600' : 'text-gray-600 hover:bg-gray-100 hover:text-indigo-600' ?>">
                     <div class="w-8 flex justify-center items-center flex-shrink-0">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
