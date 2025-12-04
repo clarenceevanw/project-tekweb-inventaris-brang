68b54d262fb03b7dc86a60dee7e2bbfe54654325
Baksoman <bryanraharjou@gmail.com>
2025-12-04 19:55:06 +0700

    fix: call subscribtion service instead of model

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -1,32 +1,33 @@
 <?php
 
-require __DIR__ . '/../Models/Barang.php';
-require __DIR__ . '/../Models/Transaksi.php';
-require __DIR__ . '/../Models/PaketSubscription.php';
+require_once __DIR__ . '/../Models/Barang.php';
+require_once __DIR__ . '/../Models/Transaksi.php';
+require_once __DIR__ . '/../Services/SubscriptionService.php';
 
 class DashboardController extends BaseController {
     private $barangModel;
     private $transaksiModel;
-    private $paketModel;
+    private $subscriptionService;
 
     public function __construct() {
         parent::__construct();
         $this->barangModel = new Barang();
         $this->transaksiModel = new Transaksi();
-        $this->paketModel = new PaketSubscription();
+        $this->subscriptionService = new SubscriptionService();
     }
     
     public function adminDashboard() {
         $id_gudang = $_SESSION['gudang']['id_gudang'];
-        
+
         $data['total_barang'] = $this->barangModel->countByGudang($id_gudang);
         $data['total_stok'] = $this->barangModel->getTotalStokByGudang($id_gudang);
         $data['total_supply'] = $this->transaksiModel->countByGudangAndType($id_gudang, 'supply');
         $data['total_buy'] = $this->transaksiModel->countByGudangAndType($id_gudang, 'buy');
         $data['transaksi_bulanan'] = $this->transaksiModel->getMonthlyTransaksi($id_gudang, 6);
         $data['top_barang'] = $this->barangModel->getTopBarangByStok($id_gudang, 5);
+        $data['paket_list'] = $this->subscriptionService->getAllPaket();
+
         $data['title'] = 'Dashboard';
-        
         return $this->view('admin/dashboard', $data);
     }
 
@@ -36,7 +37,7 @@ class DashboardController extends BaseController {
     }
 
     public function pembayaran() {
-        $data['paket_list'] = $this->paketModel->all();
+        $data['paket_list'] = $this->subscriptionService->getAllPaket();
         $data['title'] = 'Perpanjang Langganan';
         return $this->view('admin/pembayaran', $data);
     }
--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,15 +1,21 @@
 <?php
 
-require_once __DIR__ . "/../Models/Gudang.php";
+require_once __DIR__ . '/../Models/Gudang.php';
+require_once __DIR__ . '/../Services/SubscriptionService.php';
 
 class HomeController extends BaseController {
+    private $subscriptionService;
+
     public function __construct() {
         parent::__construct(new Gudang());
+        $this->subscriptionService = new SubscriptionService();
     }
 
     public function index() {
         $data["title"] = "Home";
         $data["gudang"] = $this->model->all();
+        $data["paket_subscriptions"] = $this->subscriptionService->getAllPaket();
+
         $this->flash('success', 'Welcome to the home page!');
         return $this->view("home", $data);
     }
