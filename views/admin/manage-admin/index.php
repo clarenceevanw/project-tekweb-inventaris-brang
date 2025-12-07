f2b7b3a0c89a10ea0a1dd24ce2f2425d5121af1a
clarenceevanw <clarenceevan0907@gmail.com>
2025-12-07 15:39:18 +0700

    fix: style profile photo in admin

--- views/admin/manage-admin/index.php
+++ views/admin/manage-admin/index.php
@@ -58,8 +58,8 @@
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <div class="flex items-center">
                                     <div class="flex-shrink-0 h-10 w-10">
-                                        <div class="h-10 w-10 rounded-full bg-indigo-100 flex items-center justify-center">
-                                            <svg class="h-6 w-6 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <div class="h-10 w-10 rounded-full bg-[var(--theme-secondary-lighter)] flex items-center justify-center">
+                                            <svg class="h-6 w-6 text-theme-secondary" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                                             </svg>
                                         </div>
